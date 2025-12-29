e5662a336917f72deb0e871f23081d0eed864c0e
hadjinxx <144666381+hadjicuteeee@users.noreply.github.com>
2025-12-30 03:50:54 +0800

    Update YEARLY_SALES.sql

--- TRAIN_SALES/YEARLY_SALES.sql
+++ TRAIN_SALES/YEARLY_SALES.sql
@@ -4,6 +4,7 @@ SELECT
 	WHERE EXTRACT(YEAR FROM "Ship Date") = 2015;
 	
 SELECT
+	
 	SUM(sales) as "2016 Sales"
 	FROM sales
 	WHERE EXTRACT(YEAR FROM "Ship Date") = 2016;
@@ -23,4 +24,5 @@ SELECT
 	COUNT(*) AS YEARLY
 	FROM SALES
 	GROUP BY "Year"
+
 
